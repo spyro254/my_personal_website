a9fc7532dcd3ff69d97fad0139a4273d2a66ec4a
spyro254 <36949845+spyro254@users.noreply.github.com>
2018-05-29 07:28:02 +0100

    Old nav code removed. Page overlay code removed. New nav needs to be renamed if placed on bottom for mobile.

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,3 @@
-// function toggleNav() {
-//   clicked = document.getElementById("_togglenav").parentElement;
-//   po = document.getElementById("_page-overlay");
-//   if (clicked.classList.contains("btm-nav--open")) {
-//     clicked.classList.remove("btm-nav--open");
-//     clicked.classList.add("btm-nav--closed");
-//     po.classList.remove("page-overlay--show");
-//     po.classList.add("page-overlay--hide");
-//   }
-//   else if (clicked.classList.contains("btm-nav--closed")){
-//     clicked.classList.remove("btm-nav--closed");
-//     clicked.classList.add("btm-nav--open");
-//     po.classList.remove("page-overlay--hide");
-//     po.classList.add("page-overlay--show");
-//   }
-//   else {
-//     clicked.classList.remove("btm-nav--closed");
-//     clicked.classList.add("btm-nav--open");
-//     po.classList.remove("page-overlay--hide");
-//     po.classList.add("page-overlay--show");
-//   }
-// }
-
 function expandNav() {
   sn = document.getElementById("_sidenav");
   snc = document.getElementById("_sidenav-container");
