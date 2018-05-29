ae8e8c13b90d8b4013df9f13c4dd46f3736a1e16
spyro254 <36949845+spyro254@users.noreply.github.com>
2018-05-29 09:08:41 +0100

    Nav working smoothly

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,29 @@
 function expandNav() {
   sn = document.getElementById("_sidenav");
   snc = document.getElementById("_sidenav-container");
+  abt = document.getElementById("_snb-about");
+  cod = document.getElementById("_snb-coding");
+  bra = document.getElementById("_snb-branding");
+  edu = document.getElementById("_snb-education");
   sn.style.width = "200px";
   snc.style.margin = "0 10%";
-  snc.children[0].classList.toggle("sidenav__box--open");
-  snc.children[1].classList.toggle("sidenav__box--open");
-  snc.children[2].classList.toggle("sidenav__box--open");
+  abt.classList.add("width-100");
+  cod.classList.add("width-100");
+  bra.classList.add("width-100");
+  edu.classList.add("width-100");
 }
 
 function shrinkNav() {
   sn = document.getElementById("_sidenav");
   snc = document.getElementById("_sidenav-container");
+  abt = document.getElementById("_snb-about");
+  cod = document.getElementById("_snb-coding");
+  bra = document.getElementById("_snb-branding");
+  edu = document.getElementById("_snb-education");
   sn.style.width = "80px";
   snc.style.margin = "0 30%";
-  snc.children[0].classList.toggle("sidenav__box--open");
-  snc.children[1].classList.toggle("sidenav__box--open");
-  snc.children[2].classList.toggle("sidenav__box--open");
+  abt.classList.remove("width-100");
+  cod.classList.remove("width-100");
+  bra.classList.remove("width-100");
+  edu.classList.remove("width-100");
 }
