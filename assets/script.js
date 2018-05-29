a1062e84c543bf2a2070fe8ed0726a34aa8f5b87
spyro254 <36949845+spyro254@users.noreply.github.com>
2018-05-29 07:24:05 +0100

    Designed new side nav menu. Old menu still in place. Code needs tidying

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,38 @@
-function toggleNav() {
-  clicked = document.getElementById("_togglenav").parentElement;
-  po = document.getElementById("_page-overlay");
-  if (clicked.classList.contains("btm-nav--open")) {
-    clicked.classList.remove("btm-nav--open");
-    clicked.classList.add("btm-nav--closed");
-    po.classList.remove("page-overlay--show");
-    po.classList.add("page-overlay--hide");
-  }
-  else if (clicked.classList.contains("btm-nav--closed")){
-    clicked.classList.remove("btm-nav--closed");
-    clicked.classList.add("btm-nav--open");
-    po.classList.remove("page-overlay--hide");
-    po.classList.add("page-overlay--show");
-  }
-  else {
-    clicked.classList.remove("btm-nav--closed");
-    clicked.classList.add("btm-nav--open");
-    po.classList.remove("page-overlay--hide");
-    po.classList.add("page-overlay--show");
-  }
+// function toggleNav() {
+//   clicked = document.getElementById("_togglenav").parentElement;
+//   po = document.getElementById("_page-overlay");
+//   if (clicked.classList.contains("btm-nav--open")) {
+//     clicked.classList.remove("btm-nav--open");
+//     clicked.classList.add("btm-nav--closed");
+//     po.classList.remove("page-overlay--show");
+//     po.classList.add("page-overlay--hide");
+//   }
+//   else if (clicked.classList.contains("btm-nav--closed")){
+//     clicked.classList.remove("btm-nav--closed");
+//     clicked.classList.add("btm-nav--open");
+//     po.classList.remove("page-overlay--hide");
+//     po.classList.add("page-overlay--show");
+//   }
+//   else {
+//     clicked.classList.remove("btm-nav--closed");
+//     clicked.classList.add("btm-nav--open");
+//     po.classList.remove("page-overlay--hide");
+//     po.classList.add("page-overlay--show");
+//   }
+// }
+
+function expandNav() {
+  sn = document.getElementById("_sidenav");
+  snc = document.getElementById("_sidenav-container");
+  sn.style.width = "200px";
+  sn.children[0].innerHTML = "Abdulrehmarn Ali";
+  snc.style.margin = "0 10%";
+}
+
+function shrinkNav() {
+  sn = document.getElementById("_sidenav");
+  snc = document.getElementById("_sidenav-container");
+  sn.style.width = "80px";
+  sn.children[0].innerHTML = "A Ali";
+  snc.style.margin = "0 30%";
 }
