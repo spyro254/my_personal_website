e804ea3068272bf1d4029a3502fd680810f10fb9
spyro254 <36949845+spyro254@users.noreply.github.com>
2018-05-29 08:00:47 +0100

    Cleaned up design

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@ function expandNav() {
   sn = document.getElementById("_sidenav");
   snc = document.getElementById("_sidenav-container");
   sn.style.width = "200px";
-  sn.children[0].innerHTML = "Abdulrehmarn Ali";
   snc.style.margin = "0 10%";
 }
 
@@ -10,6 +9,5 @@ function shrinkNav() {
   sn = document.getElementById("_sidenav");
   snc = document.getElementById("_sidenav-container");
   sn.style.width = "80px";
-  sn.children[0].innerHTML = "A Ali";
   snc.style.margin = "0 30%";
 }
