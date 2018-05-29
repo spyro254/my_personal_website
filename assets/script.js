cc9e620cb8d4166be64e62ffc09c77b61f418e4e
spyro254 <36949845+spyro254@users.noreply.github.com>
2018-05-29 08:31:23 +0100

    Icons added as menu items. Currently toggling side menu icons, causing them to snap. Need to use position etc

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ function expandNav() {
   snc = document.getElementById("_sidenav-container");
   sn.style.width = "200px";
   snc.style.margin = "0 10%";
+  snc.children[0].classList.toggle("sidenav__box--open");
+  snc.children[1].classList.toggle("sidenav__box--open");
+  snc.children[2].classList.toggle("sidenav__box--open");
 }
 
 function shrinkNav() {
@@ -10,4 +13,7 @@ function shrinkNav() {
   snc = document.getElementById("_sidenav-container");
   sn.style.width = "80px";
   snc.style.margin = "0 30%";
+  snc.children[0].classList.toggle("sidenav__box--open");
+  snc.children[1].classList.toggle("sidenav__box--open");
+  snc.children[2].classList.toggle("sidenav__box--open");
 }
